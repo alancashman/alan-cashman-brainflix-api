9fdd61d2573457908a7c3dbc19d69f105a28acc7
Alan Cashman <nocabbage@gmail.com>
2023-03-14 15:22:07 -0700

    add .env file

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
+const fs = require("fs");
 const express = require("express");
+const cors = require("cors");
 const { v4: uuid } = require("uuid");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-const data = require("./data/video-details.json");
-const port = 5000;
+app.use(cors({ origin: CORS_ORIGIN }));
 
-console.log(data);
+// Read video data from JSON file
+function readVideos() {
+  const videosFile = fs.readFileSync("./data/video-details.json");
+  const videosData = JSON.parse(videosFile);
+  return videosData;
+}
 
+// Write video upload to JSON file
+function writeVideo(data) {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/video-details.json", stringifiedData);
+}
+
+// GET /videos
 app.get("/videos", (req, res) => {
-  res.send(data);
+  const videos = readVideos();
+  res.status(200).send(videos);
 });
+
+// GET video
 
 app.get("/videos/:id", (req, res) => {
   const id = req.params.id;
   console.log(id);
   const video = data.find((video) => video.id === id);
-  res.send(video);
+  if (!video) {
+    res.status(404).send("We couldn't find the video in question");
+  } else {
+    res.status(200).send(video);
+  }
 });
 
 app.post("/videos", (req, res) => {
-  console.log(req.body);
-  const body = req.body;
-  const title = body.title;
-  const description = body.description;
+  const title = req.body.title;
+  const description = req.body.description;
 
   if (!title || !description) {
     res.status(400).send("Must have both a title and description");
@@ -33,14 +55,23 @@ app.post("/videos", (req, res) => {
   }
 
   const newVideo = {
-    title,
-    description,
-    posted: Date.now(),
     id: uuid(),
+    title,
+    channel: "Guest",
+    image: "image",
+    description,
+    views: 0,
+    likes: 0,
+    duration: "3:33",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: Date.now(),
+    comments: [],
   };
-  res.send(newVideo);
+  data.push(newVideo);
+  console.log(data);
+  res.status(201).send(newVideo);
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
